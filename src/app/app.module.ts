0633c5f8e33908f59ea6d00731865f9a22fcd062
Kaznarah1 <127631298+Kaznarah1@users.noreply.github.com>
2023-10-26 00:16:50 +0300

    create component acceuil and add the image in assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashbordComponent
+    DashbordComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule
