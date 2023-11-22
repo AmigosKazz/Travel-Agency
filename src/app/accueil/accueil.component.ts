38aa48e7cd078fc1bfdc4fae0324838ea31f2c83
Kaznarah1 <127631298+Kaznarah1@users.noreply.github.com>
2023-11-22 17:24:13 +0300

    fonctionnalité en generale

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit{
+  showScrollToTop = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollToTop = !!(window.scrollY || document.documentElement.scrollTop || document.body.scrollTop > 600);
+  }
+
+  scrollToElement(elementClass: string) {
+    const element = document.querySelector(`.${elementClass}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  ngOnInit() {
+
+  }
 
 }
