1e67fa20b899f90b42f023ca1ac0eb631b35d31f
Kaznarah1 <127631298+Kaznarah1@users.noreply.github.com>
2023-12-10 20:11:13 +0300

    bug fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
