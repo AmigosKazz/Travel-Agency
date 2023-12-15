33c600451eb8071ac364e93ee83e1feca5aa8ab5
Kaznarah1 <127631298+Kaznarah1@users.noreply.github.com>
2023-12-15 23:14:41 +0300

    reservation model

---
+++ src/app/reservation/model/reservation.ts
@@ -0,0 +1,21 @@
+export interface Utilisateur {
+  id_utilisateur: number;
+  nom_utilisateur: string;
+  email_utilisateur: string;
+  password_utilisateur: string;
+  role_utilisateur: string;
+}
+
+export interface Destination {
+  id_destination: number;
+  nom_destination: string;
+  description_destination: string;
+  prix_destination: number;
+}
+export interface Reservation {
+   utilisateur: Utilisateur;
+    destination: Destination;
+    date_depart: Date;
+    date_retour: Date;
+    nombre_personne: number;
+}
